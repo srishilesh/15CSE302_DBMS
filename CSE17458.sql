602d9ddf41c9c0d7b6227f87cc7c9c1c73b89546
Srishilesh P S <32015336+srishilesh@users.noreply.github.com>
2019-06-19 21:08:53 +0530

    Update CSE17458.sql

--- CSE17458.sql
+++ CSE17458.sql
@@ -1,52 +1,57 @@
--- CREATE A DEPARTMENT TABLE
-create table department
-(dept_name varchar(20),
-building varchar(20),
-budget numeric(12,2),
-primary key(dept_name));
+select name from instructor;
+
+select dept_name from instructor;
+
+select name,salary from instructor;
 
 
--- CREATE A COURSE TABLE
-create table course
-(course_id varchar(7),
-title varchar(50),
-dept_name varchar(20),
-credits numeric(2,0),
-primary key(course_id),
-foreign key(dept_name) references department);
+select name,salary*1.1 from instructor;
 
--- CREATE A INSTRUCTOR TABLE
-create table instructor
-(ids varchar(5),
-names varchar(20) not null,
-dept_name varchar(20),
-salary numeric(8,2),
-primary key(ids),
-foreign key(dept_name) references department);
-
-insert into instructor values('2222','Einstein','Physics',95000);
-
--- CREATE A SECTION TABLE
-create table section
-(course_id varchar(8),
-sec_id varchar(8),
-semester varchar(6),
-years numeric(4,0),
-building varchar(15),
-room_number varchar(7),
-time_slot_id varchar(4),
-primary key(course_id,sec_id,semester,years),
-foreign key(course_id) references course);
-
--- CREATE A TEACHES TABLE
-create table teaches
-(ids varchar(5),
-course_id varchar(8),
-sec_id varchar(8),
-semester varchar(6),
-years numeric(4,0),
-primary key(ids,course_id,sec_id,semester,years),
-foreign key(course_id,sec_id,semester,years) references section,
-foreign key(ids) references instructor);
+select name from instructor where dept_name='Comp. Sci.' and salary>7000;
 
 
+select name,instructor.dept_name,building from instructor,department where instructor.dept_name = department.dept_name;
+
+alter table instructor add address varchar(20);
+desc instructor;
+alter table instructor drop column address;
+desc instructor;
+
+select name,ID from instructor natural join teaches;
+
+select name,title from instructor natural join teaches,course where teaches.course_id=course.course_id;
+
+select name,title from instructor natural join teaches natural join course;
+
+select name,title from (instructor natural join teaches) natural join course using(course_id);
+
+select name as instructor_name from instructor;
+
+select T.name, S.course_id from instructor T, teaches S where T.id = S.id;
+
+select distinct T.name from instructor S,instructor T where T.salary>S.salary and S.dept_name='Biology';
+
+
+select '--'||dept_name||'--'||building||'--' from department;
+
+select substr(dept_name,3,4) from department;
+
+select length(dept_name) from department;
+
+select lower(dept_name) from department;
+
+select upper(dept_name) from department;
+
+select trim(dept_name) from department;
+
+
+select name from instructor where name like 'Sri%';
+
+select instructor.* from instructor,teaches where instructor.id=teaches.id;
+
+
+select name from instructor where dept_name is not null;
+
+select max(salary) from instructor;
+
+select min(salary) from instructor;
